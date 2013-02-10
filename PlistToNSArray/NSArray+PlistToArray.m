d846f8bdc0c4dfe78e2dd5fbcb3c2001c944bcc2
Andrea Martino <amandreamartino@gmail.com>
2013-02-11 00:09:50 +0100

    Added few comments

--- PlistToNSArray/NSArray+PlistToArray.m
+++ PlistToNSArray/NSArray+PlistToArray.m
@@ -12,28 +12,28 @@
 
 +(NSArray*)initArrayFromPlist:(NSString*)fileName withExtension:(NSString*)extension andForAllContentWithKey:(NSString*)keyString {
     
-    NSArray *returnedArray = [[NSArray alloc] init];
-    NSError *error;
+    NSArray *returnedArray = [[NSArray alloc] init]; //alloc & init
+    NSError *error; //store error
     
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSFileManager *fileManager = [NSFileManager defaultManager]; 
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES); //scan for Document folder and return path
     
     NSString *plistName = [NSString stringWithFormat:@"%@.%@", fileName, extension];
     
-    NSString *plistPathFinal = [[paths objectAtIndex:0] stringByAppendingPathComponent:plistName];
-    NSString *plistPathStart = [[NSBundle mainBundle] pathForResource:fileName ofType:extension];
+    NSString *plistPathFinal = [[paths objectAtIndex:0] stringByAppendingPathComponent:plistName]; //plist initial location (resource file folder)
+    NSString *plistPathStart = [[NSBundle mainBundle] pathForResource:fileName ofType:extension]; //plist final location
     
-    NSDictionary *plistDictionary = [NSDictionary dictionaryWithContentsOfFile:plistPathFinal];
+    NSDictionary *plistDictionary = [NSDictionary dictionaryWithContentsOfFile:plistPathFinal]; 
     
-    if ([fileManager fileExistsAtPath:plistPathFinal]==NO) {
+    if ([fileManager fileExistsAtPath:plistPathFinal]==NO) { //if file doesn't exist
         
-        [fileManager copyItemAtPath:plistPathStart toPath:plistPathFinal error:&error];
+        [fileManager copyItemAtPath:plistPathStart toPath:plistPathFinal error:&error]; //copy plist in document folder
          NSLog(@"Done");
         
     }
     
     
-    returnedArray = [plistDictionary valueForKey:keyString];
+    returnedArray = [plistDictionary valueForKey:keyString]; 
     
     return returnedArray;
     
